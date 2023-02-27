d1600a0ae913fcba862295cafd21429cd9c34bf9
flafflar <achmichail@gmail.com>
2023-02-27 16:26:34 +0200

    Added a CMake build script
    This enables cross-platform building without needing Projucer to
    generate the build files.

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -28,7 +28,7 @@
 
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
 
 //==============================================================================
 /**
