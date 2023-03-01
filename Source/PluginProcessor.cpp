flafflar/FaderVST

commit 5921b6c1e1d78cd45153677e75025816892bf64b

Temporarily removed a problematic piece of code
The gain applied after the end of the ramp caused some audible
artifacts, and could sometimes make the plugin crash. This block was
removed temporarily in order to figure out what's happening with it
later in development.

Source/PluginProcessor.cpp
@@ -147,7 +147,11 @@ void FaderVSTAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    if (fadeDuration == 0) return;
+    if (fadeDuration == 0){
+        // Just apply constant gain and end the processing
+        buffer.applyGain(gain);
+        return;
+    };
     
     /** How many samples remain until the fading ends */
     int remaining;
@@ -177,9 +181,10 @@ void FaderVSTAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     buffer.applyGainRamp(0, buffer.getNumSamples(), gain, finalGain);
 
     // If any samples remain after the ramp, apply a constant gain
-    if (samplesToProcess < buffer.getNumSamples()){
+    // TODO: Make this work properly
+    /*if (samplesToProcess < buffer.getNumSamples()){
         buffer.applyGain(samplesToProcess, buffer.getNumSamples(), finalGain);
-    }
+    }*/
 
     gain = finalGain;
 }

commit 429e8c4c4c772687abc8bf74645d79f57220440f

Fixed the bug introduced in commit 5921b6c

Source/PluginProcessor.cpp
@@ -202,13 +202,12 @@ void FaderVSTAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     }
 
     // Apply the gain ramp
-    buffer.applyGainRamp(0, buffer.getNumSamples(), *gain, finalGain);
+    buffer.applyGainRamp(0, samplesToProcess, *gain, finalGain);
 
     // If any samples remain after the ramp, apply a constant gain
-    // TODO: Make this work properly
-    /*if (samplesToProcess < buffer.getNumSamples()){
-        buffer.applyGain(samplesToProcess, buffer.getNumSamples(), finalGain);
-    }*/
+    if (samplesToProcess < buffer.getNumSamples()){
+        buffer.applyGain(samplesToProcess, buffer.getNumSamples() - samplesToProcess, finalGain);
+    }
 
     // Notify the editor that the parameter has changed so it can update the GUI
     parameters.getParameter("gain")->setValueNotifyingHost(finalGain);